f6adc4383dfc5c022f9bdc0e83f9efc493b93e5e
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-02-23 03:48:54 +0100

    0-rotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,13 @@
 def rotate_2d_matrix(matrix):
-    """
-    Rotates a 2D matrix 90 degrees clockwise.
-    """
+    """Rotates an m by n 2D matrix in place."""
+    if not isinstance(matrix, list) or not matrix or not all(isinstance(row, list) for row in matrix):
+        return
+
     n = len(matrix)
-    
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-    # Reverse each row
-    for row in matrix:
-        row.reverse()
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
