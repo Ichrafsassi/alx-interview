d4fdea6bf00755a1cd32361cd5da4459c7b66995
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-03-30 17:59:28 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+def check_two_queens(new_queen_position, other_queen_position):
+    if new_queen_position[0] == other_queen_position[0] or new_queen_position[1] == other_queen_position[1]:
+        return False
+    if new_queen_position[0] + new_queen_position[1] == other_queen_position[0] + other_queen_position[1]:
+        return False
+    if new_queen_position[0] - new_queen_position[1] == other_queen_position[0] - other_queen_position[1]:
+        return False
+    return True
+
 def is_safe(board, row, col):
     for i in range(row):
         if board[i] == col or abs(board[i] - col) == row - i:
@@ -17,14 +26,14 @@ def solve_nqueens(N, board, row, solutions):
             board[row] = col
             solve_nqueens(N, board, row + 1, solutions)
 
-def print_solutions(N):
-    if not N.isdigit():
-        print("N must be a number", file=sys.stderr)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         sys.exit(1)
 
-    N = int(N)
+    N = int(sys.argv[1])
     if N < 4:
-        print("N must be at least 4", file=sys.stderr)
+        print("N must be at least 4")
         sys.exit(1)
 
     board = [-1] * N
@@ -33,11 +42,3 @@ def print_solutions(N):
 
     for solution in solutions:
         print(solution)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N", file=sys.stderr)
-        sys.exit(1)
-
-    N = sys.argv[1]
-    print_solutions(N)
