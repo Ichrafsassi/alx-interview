2f1c35289f6dccc7e13771769c69872164b293f2
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-03-30 18:01:56 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 import sys
 
-def check_two_queens(new_queen_position, other_queen_position):
-    if new_queen_position[0] == other_queen_position[0] or new_queen_position[1] == other_queen_position[1]:
-        return False
-    if new_queen_position[0] + new_queen_position[1] == other_queen_position[0] + other_queen_position[1]:
-        return False
-    if new_queen_position[0] - new_queen_position[1] == other_queen_position[0] - other_queen_position[1]:
-        return False
-    return True
-
 def is_safe(board, row, col):
+    # Check if there is a queen in the same column
     for i in range(row):
-        if board[i] == col or abs(board[i] - col) == row - i:
+        if board[i] == col:
             return False
+
+        # Check upper diagonal on left side
+        if board[i] - i == col - row:
+            return False
+
+        # Check upper diagonal on right side
+        if board[i] + i == col + row:
+            return False
+
     return True
 
 def solve_nqueens(N, board, row, solutions):
     if row == N:
-        solutions.append(board[:])
+        solutions.append(list(enumerate(board)))
         return
 
     for col in range(N):
@@ -26,12 +27,12 @@ def solve_nqueens(N, board, row, solutions):
             board[row] = col
             solve_nqueens(N, board, row + 1, solutions)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+def print_solutions(N):
+    if not N.isdigit():
+        print("N must be a number")
         sys.exit(1)
 
-    N = int(sys.argv[1])
+    N = int(N)
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
@@ -42,3 +43,11 @@ if __name__ == "__main__":
 
     for solution in solutions:
         print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    N = sys.argv[1]
+    print_solutions(N)
