c6343e062a07c408c789757e43e325996391aad2
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-03-30 18:05:01 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,12 +19,12 @@ def solve_nqueens(N, board, row, solutions):
 
 def print_solutions(N):
     if not N.isdigit():
-        print("N must be a number", file=sys.stderr)
+        print("N must be a number")
         sys.exit(1)
 
     N = int(N)
     if N < 4:
-        print("N must be at least 4", file=sys.stderr)
+        print("N must be at least 4")
         sys.exit(1)
 
     board = [-1] * N
@@ -36,7 +36,7 @@ def print_solutions(N):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N", file=sys.stderr)
+        print("Usage: nqueens N")
         sys.exit(1)
 
     N = sys.argv[1]
