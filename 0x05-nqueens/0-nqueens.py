2e8be9da64e3ed84995bb252b9c371066d929208
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-03-30 17:36:19 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,53 +1,63 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col):
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i] == col:
-            return False
+possibilties = []
+for line in sys.stdin:
+    try:
+        possibilties.append(eval(line))
+    except Exception as e:
+        pass
 
-        # Check upper diagonal on left side
-        if board[i] - i == col - row:
-            return False
-
-        # Check upper diagonal on right side
-        if board[i] + i == col + row:
-            return False
-
+def check_two_queens(new_queen_position, other_queen_position):
+    if new_queen_position[0] == other_queen_position[0] or new_queen_position[1] == other_queen_position[1]:
+        return False
+    if new_queen_position[0] + new_queen_position[1] == other_queen_position[0] + other_queen_position[1]:
+        return False
+    if new_queen_position[0] - new_queen_position[1] == other_queen_position[0] - other_queen_position[1]:
+        return False
     return True
 
-def solve_nqueens(N, board, row, solutions):
-    if row == N:
-        solutions.append(list(enumerate(board)))
-        return
 
-    for col in range(N):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(N, board, row + 1, solutions)
+def possibilty_already_done(possibilty, possibilties_done):
+    if len(possibilties_done) == 0:
+        return False
+    for possibilty_done in possibilties_done:
+        nb_similar = 0
+        for position in possibilty:
+            nb_similar += 1 if position in possibilty_done else 0
+        if nb_similar == len(possibilty_done):
+            return True
+    return False
 
-def print_solutions(N):
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
+result = True
+possibilties_done = []
+for possibilty in possibilties:
+    i = 0
+    while i < len(possibilty) -1:
+        j = i + 1
+        while j < len(possibilty):
+            result = check_two_queens(possibilty[i], possibilty[j])
+            if not result:
+                break
+            j += 1
+        i += 1
+        if not result:
+            break
+    if not result:
+        break
+    else:
+        # good possibilty
+        if possibilty_already_done(possibilty, possibilties_done):
+            result = False
+        else:
+            possibilties_done.append(possibilty)
 
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    if not result:
+        break
+    
 
-    board = [-1] * N
-    solutions = []
-    solve_nqueens(N, board, 0, solutions)
-
-    for solution in solutions:
-        print(solution)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    N = sys.argv[1]
-    print_solutions(N)
+if result:
+    print("OK")
+else:
+    print("NOK")
+print(len(possibilties))
